9e686fbffa6b414bdb25a1684805550cffd59a64
Aleksandar Pajevic <aleksandar.pajevic@gmail.com>
2021-06-23 22:58:01 +0200

    nav active class

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function whosActive(e) {
+  let elems = document.querySelector('.activeNav');
+  if (elems !== null) {
+    elems.classList.remove('activeNav');
+  }
+  e.target.className = 'activeNav';
+}
+
 function makeMeFocus() {
-  document.getElementById('home').focus();
+  document.getElementById('home').classList.add('activeNav');
 }
