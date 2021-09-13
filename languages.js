f3a59bd0101e7451c7f858abf41e4f85a0fab56b
Aleksandar Pajevic <aleksandar.pajevic@gmail.com>
2021-09-13 18:20:22 +0200

    basic language change

--- languages.js
+++ languages.js
@@ -3,3 +3,16 @@
 import langData from './languagesData.json' assert { type: 'json' };
 
 console.log('lang data =>', langData);
+
+function applyStrings() {
+  document.querySelectorAll('[data-key]').forEach((element) => {
+    console.log('element', element);
+    let key = element.getAttribute('data-key');
+    console.log('key', key);
+    if (key) {
+      element.textContent = langData.languages['sr'].strings[key];
+    }
+  });
+}
+
+applyStrings();
