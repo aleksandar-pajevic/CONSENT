85d7b7c8a3b3ba3d2a007c14cc703e1a13890b65
aleksandar-pajevic <aleksandar.pajevic@gmail.com>
2021-11-01 18:30:32 +0100

    new homepage

--- index.js
+++ index.js
@@ -26,13 +26,13 @@ function opacityInc(overlay) {
   let overlays = document.getElementsByClassName(overlay);
 
   for (var i = 0; i < overlays.length; i++) {
-    overlays[i].style.opacity = '0.5';
+    overlays[i].style.opacity = '0.4';
   }
 }
 function opacityDec(overlay) {
   let overlays = document.getElementsByClassName(overlay);
 
   for (var i = 0; i < overlays.length; i++) {
-    overlays[i].style.opacity = '0.4';
+    overlays[i].style.opacity = '0.3';
   }
 }
